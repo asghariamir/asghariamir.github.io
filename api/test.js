11aa818c9fe044c3c5509238b015fb2e791b6232
asghariamir <88145479+asghariamir@users.noreply.github.com>
2025-08-20 15:21:57 +0100

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,11 +1,6 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  
-  res.status(200).json({ 
+module.exports = (req, res) => {
+  res.status(200).json({
     success: true,
-    message: 'API is working!',
-    timestamp: new Date().toISOString(),
-    method: req.method
+    message: 'API working with module.exports!'
   });
-}
+};
