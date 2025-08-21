7304bfa6bc0d43e781e2fc8e0331bf8fee9b10c0
asghariamir <88145479+asghariamir@users.noreply.github.com>
2025-08-21 18:22:10 +0100

    Delete api directory

--- api/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    message: 'API root working!',
-    endpoints: ['/api/test', '/api/visitor-count']
-  });
-};
--- api/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    success: true,
-    message: 'Test API working!',
-    timestamp: new Date().toISOString()
-  });
-};
